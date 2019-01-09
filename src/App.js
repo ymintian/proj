53f153d02b1ea452a4a6d53ce619f44c8824a638
ymintian <ymintian@cogniance.com>
2019-01-09 17:18:50 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,78 @@ import './App.css';
 import fire from './my_config';
 import Test from './s';
 import Header from './header.js';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+function BasicExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/table">Table</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={App} />
+        <Route path="/table" component={Table} />
+      </div>
+    </Router>
+  );
+}
+
+
+
+class Table extends Component {
+  constructor(props){
+    super(props);
+    this.state = {teams_images:[]};
+  }
+  componentWillMount(){
+    fetch('http://api.football-data.org/v2/competitions/PL/teams',
+      {
+        headers: { 'X-Auth-Token': '7b2ac51349fd45cab94bd34a5e8db4a5' },
+        method: "GET"
+      })
+      .then((res)=>{
+        return res.json();
+        
+      })
+      .then((r)=>{console.log(r.teams);this.setState({teams_images:r.teams});})
+      .catch((er)=>{console.log("error:"+er)});
+  }
+  render(){
+    let l = this.state.teams_images.length;
+    let src = this.state.teams_images.slice(0,l);
+    
+    let ar1 = [];
+    src.forEach((el,i)=>{
+      ar1.push(<Img src={el.crestUrl} key={i}/>);
+    });
+    console.log('ar1',ar1);
+    return (
+      <div>
+        <h2>EPL Table</h2>
+        <div>{ar1}</div>
+      </div>
+    );
+  }
+}
+
+class Img extends Component {
+  render(){
+    return (
+      <div className="team_img_container">
+        <img src={this.props.src}/>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   constructor(props) {
@@ -46,4 +118,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default BasicExample;
